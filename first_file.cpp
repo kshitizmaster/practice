b4d082f46817cb9b90cb7c45e3963e70031490a3
Tushar Gupta <sharvan.kshitiz@gmail.com>
2020-04-04 13:10:13 +0530

    first_file.cpp

---
+++ first_file.cpp
@@ -0,0 +1,20 @@
+#include<iostream>
+#include<math.h>
+#include <bits/stdc++.h>
+using namespace std;
+ void swap(int *a, int *b)
+ {
+ 	int temp= *a;
+ 	*a = *b;
+ 	*b = temp;
+ }
+ int main()
+ {
+ 	cout<<"Enter the two numbers"<<endl;
+ 	int a, b;
+ 	cin>>a>>b;
+ 	swap(&a, &b);
+ 	cout<<"After swappping the values are";
+ 	cout<<endl;
+ 	cout<<a<<b;
+ }
